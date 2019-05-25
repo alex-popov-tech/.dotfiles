c3d09cdacdcc66656a5c5561c5962b106cb1b417
Alexander Popov <alex.popov.tech@gmail.com>
2019-05-26 00:04:27 +0300

    pull latest master in install scripts

--- install/managers.sh
+++ install/managers.sh
@@ -3,6 +3,7 @@
 function main() {
   [ ! -d $HOME/.dotfiles ] && git clone https://github.com/aleksanderpopov/.dotfiles.git $HOME/.dotfiles
   cd $HOME/.dotfiles
+  git pull
 
   chmod +x ./scripts/managers.sh && ./scripts/managers.sh
 }
