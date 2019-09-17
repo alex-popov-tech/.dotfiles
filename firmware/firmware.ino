f0eb02c96ea69d43f4dd6236e09ae84bb7ac63c2
Alexander Popov <alex.popov.tech@gmail.com>
2019-09-17 14:48:36 +0300

    update keyboardio layouts

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -26,68 +26,53 @@
 
 enum { MACRO_TOGGLE_LANG, MACRO_SCREEN };
 
-enum { WORKMAN, DIGITS_AND_PUNCTUATION, MOVEMENT, QWERTY };
+enum { WORKMAN, MOVEMENT_AND_PUNCTUATION, QWERTY };
 
 // *INDENT-OFF*
 
 KEYMAPS(
   [WORKMAN] = KEYMAP_STACKED (
-       ___,        M(MACRO_SCREEN), LSHIFT(Key_Backtick), LSHIFT(Key_Slash), LSHIFT(Key_3), ___,   Key_Mute,
-       Key_Tab,    Key_Q,           Key_D,                Key_R,             Key_W,         Key_B, Key_VolumeUp,
-                   Key_Escape,      Key_A,                Key_S,             Key_H,         Key_T, Key_G,
-       ___,        Key_Z,           Key_X,                Key_M,             Key_C,         Key_V, Key_VolumeDown,
+       ___,             Key_1, Key_2, Key_3, Key_4, Key_5, Key_Mute,
+       Key_Backtick,    Key_Q, Key_D, Key_R, Key_W, Key_B, Key_VolumeUp,
+       Key_Tab,         Key_A, Key_S, Key_H, Key_T, Key_G,
+       M(MACRO_SCREEN), Key_Z, Key_X, Key_M, Key_C, Key_V, Key_VolumeDown,
        CTL_T(Escape), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
-       LockLayer(DIGITS_AND_PUNCTUATION),
+       ShiftToLayer(MOVEMENT_AND_PUNCTUATION),
 
-       Key_LEDEffectNext,    ___,   ___,   ___,   LSHIFT(Key_6), ___,    ___,
-       ___,                  Key_J, Key_F, Key_U, Key_P,         TD(0),  TD(1),
-                             Key_Y, Key_N, Key_E, Key_O,         Key_I,  TD(2),
-       M(MACRO_TOGGLE_LANG), Key_K, Key_L, TD(3), TD(4),         TD(5),  ___,
-       OSM(LeftShift), OSM(LeftAlt), Key_Spacebar, CTL_T(Enter),
-       LockLayer(MOVEMENT)
+       Key_LEDEffectNext,    Key_6, Key_7, Key_8, Key_9, Key_0, ___,
+       ___,                  Key_J, Key_F, Key_U, Key_P, TD(0), TD(1),
+                             Key_Y, Key_N, Key_E, Key_O, Key_I, TD(2),
+       M(MACRO_TOGGLE_LANG), Key_K, Key_L, TD(3), TD(4), TD(5), ___,
+       OSM(LeftAlt), OSM(LeftShift), Key_Spacebar, CTL_T(Enter),
+       ShiftToLayer(MOVEMENT_AND_PUNCTUATION)
     ),
-  [DIGITS_AND_PUNCTUATION] = KEYMAP_STACKED (
-       ___, ___,               ___,             ___,                     ___,               ___,   ___,
-       ___, LSHIFT(Key_Minus), Key_Slash,       Key_Minus,               LSHIFT(Key_5),     ___,   ___,
-       ___, Key_1,             Key_2,           Key_3,                   Key_4,             Key_5,
-       ___, Key_Backslash,     Key_LeftBracket, LSHIFT(Key_LeftBracket), LSHIFT(Key_Comma), ___,   ___,
+  [MOVEMENT_AND_PUNCTUATION] = KEYMAP_STACKED (
+       ___, ___,           ___,              ___,           ___,               ___, ___,
+       ___, ___,           Key_mouseScrollR, Key_mouseUp,   Key_mouseScrollDn, ___, ___,
+       ___, Key_mouseBtnR, Key_mouseL,       Key_mouseDn,   Key_mouseR,        Key_mouseBtnL,
+       ___, ___,           Key_mouseScrollL, Key_mouseBtnM, Key_mouseScrollUp, ___, ___,
        ___, ___, ___, ___,
        ___,
 
-       ___, ___,   ___,                ___,                      ___,              ___,                   ___,
-       ___, ___,   Key_Equals,         LSHIFT(Key_Equals),       LSHIFT(Key_8),    ___, ___,
-            Key_6, Key_7,              Key_8,                    Key_9,            Key_0,                 ___,
-       ___, ___,   LSHIFT(Key_Period), LSHIFT(Key_RightBracket), Key_RightBracket, ___,         ___,
-       ___, ___,   ___, ___,
-       ___
-    ),
-  [MOVEMENT] = KEYMAP_STACKED (
-       ___, ___, ___, ___, ___, ___, ___,
-       ___, ___,           Key_mouseWarpNW, Key_mouseUp,      Key_mouseWarpNE, ___,           ___,
-       ___, Key_mouseBtnR, Key_mouseL,      Key_mouseDn,      Key_mouseR,      Key_mouseBtnL,
-       ___, ___,           Key_mouseWarpSW, Key_mouseWarpEnd, Key_mouseWarpSE, ___,           ___,
-       ___, ___, ___, ___,
-       ___,
-
-       ___, ___, ___, ___, ___, ___, ___,
-       ___, ___, ___, ___, ___, ___, ___,
-            Key_LeftArrow,    Key_DownArrow,     Key_UpArrow,       Key_RightArrow,   ___, ___,
-       ___, Key_mouseScrollL, Key_mouseScrollDn, Key_mouseScrollUp, Key_mouseScrollR, ___, ___,
+       ___, ___,           ___,           ___,         ___,            ___, ___,
+       ___, ___,           ___,           ___,         ___,            ___, ___,
+            Key_H,         Key_J,         Key_K,       Key_L,          ___, ___,
+       ___, Key_LeftArrow, Key_DownArrow, Key_UpArrow, Key_RightArrow, ___, ___,
        ___, ___, ___, ___,
        ___
     ),
   [QWERTY] = KEYMAP_STACKED (
-       ___,           ___,   ___,   ___,   ___,   ___,   ___,
-       Key_Backslash, Key_Q, Key_W, Key_E, Key_R, Key_T, ___,
-                      ___,   Key_A, Key_S, Key_D, Key_F, Key_G,
-       ___,           Key_Z, Key_X, Key_C, Key_V, Key_B, ___,
+       ___, ___,   ___,   ___,   ___,   ___,   ___,
+       ___, Key_Q, Key_W, Key_E, Key_R, Key_T, ___,
+            Key_A, Key_S, Key_D, Key_F, Key_G, ___,
+       ___, Key_Z, Key_X, Key_C, Key_V, Key_B, ___,
        ___, ___, ___, ___,
        ___,
 
        ___,  ___,   ___,   ___,       ___,           ___,           ___,
-       ___,  Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
+       ___,  Key_Y, Key_U, Key_I,     Key_O,         Key_P,         ___,
              Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-       ___,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightBracket,
+       ___,  Key_N, Key_M, Key_Comma, Key_Period,    ___,           ___,
        ___, ___, ___, ___,
        ___
     )
@@ -115,7 +100,7 @@ void tapDanceAction(uint8_t tap_dance_index, byte row, byte col, uint8_t tap_cou
     case 1:
       return tapDanceActionKeys(tap_count, tap_dance_action, LSHIFT(Key_2), LSHIFT(Key_3));
     case 2:
-      return tapDanceActionKeys(tap_count, tap_dance_action, LSHIFT(Key_Semicolon), Key_Semicolon);
+      return tapDanceActionKeys(tap_count, tap_dance_action, Key_Semicolon, LSHIFT(Key_Semicolon));
     case 3:
       return tapDanceActionKeys(tap_count, tap_dance_action, LSHIFT(Key_9), Key_Quote);
     case 4:
