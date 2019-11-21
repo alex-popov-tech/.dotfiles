efa3020fc1f876d47e8e9b281d1ecd71165e90fb
Alexander Popov <alex.popov.tech@gmail.com>
2019-11-21 11:59:51 +0200

    update led's settings

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,9 +10,8 @@
 #include "Kaleidoscope-FocusSerial.h"
 #include "Kaleidoscope-HostOS.h"
 #include "Kaleidoscope-LED-Palette-Theme.h"
-#include "Kaleidoscope-LEDEffect-Breathe.h"
-#include "Kaleidoscope-LED-Stalker.h"
 #include "Kaleidoscope-Colormap.h"
+#include "Kaleidoscope-IdleLEDs.h"
 #include "Kaleidoscope-Syster.h"
 
 #include "Kaleidoscope-Macros.h"
@@ -27,14 +26,14 @@ enum { COLEMAK };
 // *INDENT-OFF*
 KEYMAPS(
   [COLEMAK] = KEYMAP_STACKED (
-    XXX,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_VolumeUp,
+    Key_LEDEffectNext,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_VolumeUp,
     Key_LBracket, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_VolumeDown,
     Key_Tab,      Key_A, Key_R, Key_S, Key_T, Key_D,
     Key_Backtick, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Equals,
     Key_Enter, Key_Backspace, Key_LeftGui, Key_LeftControl,
     Key_LeftShift,
 
-    Key_Mute,        Key_6, Key_7, Key_8,     Key_9,      Key_0,     Key_LEDEffectNext,
+    Key_Mute,        Key_6, Key_7, Key_8,     Key_9,      Key_0,     XXX,
     M(MACRO_SCREEN), Key_J, Key_L, Key_U,     Key_Y,      Key_Quote, Key_RBracket,
                      Key_H, Key_N, Key_E,     Key_I,      Key_O,     Key_Minus,
     Key_Semicolon,   Key_K, Key_M, Key_Comma, Key_Period, Key_Slash, Key_Backslash,
@@ -55,14 +54,13 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 
 KALEIDOSCOPE_INIT_PLUGINS(
   LEDControl,
+  LEDOff,
   HostOS,
   EEPROMSettings,
   EEPROMKeymap,
-  ColormapEffect,
   LEDPaletteTheme,
-  LEDOff,
-  LEDBreatheEffect,
-  StalkerEffect,
+  IdleLEDs,
+  ColormapEffect,
 
   Syster,
   Focus,
@@ -76,9 +74,8 @@ void setup() {
   ColormapEffect.max_layers(1);
   ColormapEffect.activate();
 
-  // red haunt after press
-  StalkerEffect.variant = STALKER(Haunt, (CRGB(255, 0, 0)));
-  StalkerEffect.activate();
+  // turn off led's after 10 minutes of inactivity
+  IdleLEDs.setIdleTimeoutSeconds(10 * 60);
   // To make the keymap editable without flashing new firmware, we store
   // additional layers in EEPROM. For now, we reserve space for five layers. If
   // one wants to use these layers, just set the default layer to one in EEPROM,
