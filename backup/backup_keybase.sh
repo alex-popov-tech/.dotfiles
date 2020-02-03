c7472883bd4e27246bec73180e3716e6a2d77259
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-04 01:00:37 +0200

    update backup script

--- backup/backup_keybase.sh
+++ backup/backup_keybase.sh
@@ -1,11 +1,10 @@
 #! /usr/bin/env sh
 
 set -e
-set -v
 
 cd $($HOME/.asdf/shims/node $HOME/.dotfiles/backup/backup_keybase.js)
 name=keybase_$(date '+%F')
 tar -czf $name.tar ./keybase
 rm -rf ./keybase
-cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
+export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
 rm ./$name.tar
