f7a5e77f7bcaf524e68158f2c2aa2d89d18d18c4
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-04 01:06:11 +0200

    update backup scripts

--- backup/backup_keybase.sh
+++ backup/backup_keybase.sh
@@ -8,3 +8,6 @@ tar -czf $name.tar ./keybase
 rm -rf ./keybase
 export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
 rm ./$name.tar
+
+# to decrypt
+# gpg -o ./archive.tar.gzip -d ./keybase_2020-02-04.gzip.gpg
--- backup/backup_lastpass.sh
+++ backup/backup_lastpass.sh
@@ -4,3 +4,6 @@ set -e
 
 data="$(/usr/local/bin/lpass export)"
 export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/lpass_$(date '+%F').gzip.gpg
+
+# to decrypt
+# gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
