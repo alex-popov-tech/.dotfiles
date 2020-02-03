87c473cd15a8e9ac4946db202c0b682013047bfd
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-04 01:28:00 +0200

    add backups on keybase

--- backup/backup_lastpass.sh
+++ backup/backup_lastpass.sh
@@ -3,7 +3,8 @@
 set -e
 
 data="$(/usr/local/bin/lpass export)"
-export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/lpass_$(date '+%F').gzip.gpg
-
+filename=lpass_"$(date '+%F')".gzip.gpg
+export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/"$filename"
 # to decrypt
 # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
+cat "/Volumes/BACKUP/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
