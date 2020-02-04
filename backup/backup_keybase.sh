2c5192190f8c5c51203101c76b7a681c173aea2a
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-04 16:48:27 +0200

    add if backup exists check

--- backup/backup_keybase.sh
+++ backup/backup_keybase.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-cd $($HOME/.asdf/shims/node $HOME/.dotfiles/backup/backup_keybase.js)
 name=keybase_$(date '+%F')
-tar -czf $name.tar ./keybase
-rm -rf ./keybase
-export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
-rm ./$name.tar
-# to decrypt
-# gpg -o ./archive.tar.gzip -d ./keybase_2020-02-04.gzip.gpg
-cat "/Volumes/BACKUP/$name.gzip.gpg" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$name.gzip.gpg"
+
+if [ ! -f /Volumes/BACKUP/$name.gzip.gpg ]; then
+  cd $($HOME/.asdf/shims/node $HOME/.dotfiles/backup/backup_keybase.js)
+  tar -czf $name.tar ./keybase
+  rm -rf ./keybase
+  export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
+  rm ./$name.tar
+  # to decrypt
+  # gpg -o ./archive.tar.gzip -d ./keybase_2020-02-04.gzip.gpg
+  cat "/Volumes/BACKUP/$name.gzip.gpg" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$name.gzip.gpg"
+fi
+
+
--- backup/backup_lastpass.sh
+++ backup/backup_lastpass.sh
@@ -4,7 +4,11 @@ set -e
 
 data="$(/usr/local/bin/lpass export)"
 filename=lpass_"$(date '+%F')".gzip.gpg
-export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/"$filename"
-# to decrypt
-# gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
-cat "/Volumes/BACKUP/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+
+if [ ! -f /Volumes/BACKUP/"$filename" ]; then
+  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/"$filename"
+  # to decrypt
+  # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
+  cat "/Volumes/BACKUP/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+fi
+
