9e48ab615bd4678df155018aed9efa8116a2ed15
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-10 13:35:49 +0200

    add backup on remote usb

--- backup/backup_keybase.sh
+++ backup/backup_keybase.sh
@@ -4,15 +4,20 @@ set -e
 
 name=keybase_$(date '+%F')
 
-if [ ! -f /Volumes/BACKUP/$name.gzip.gpg ]; then
-  cd $($HOME/.asdf/shims/node $HOME/.dotfiles/backup/backup_keybase.js)
+if [ "ssh admin@192.168.31.1 stat /media/BACKUP/$name.tar \> /dev/null 2\>\&1" ]; then
+  path=$($HOME/.asdf/shims/node $HOME/.dotfiles/backup/backup_keybase.js)
+  cd $path
   tar -czf $name.tar ./keybase
   rm -rf ./keybase
-  export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/$name.gzip.gpg
+  export LC_ALL=en_US.UTF-8 && cat ./$name.tar | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "./$name.tar.gzip.gpg"
   rm ./$name.tar
+  # to download
+  # scp admin@192.168.31.1:/media/BACKUP/keybase_2020-02-10.tar.gzip.gpg ./keybase.tar.gzip.gpg
+  scp "./$name.tar.gzip.gpg" admin@192.168.31.1:/media/BACKUP/
+  cat "./$name.tar.gzip.gpg" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$name.gzip.gpg"
+  rm "./$name.tar.gzip.gpg"
   # to decrypt
-  # gpg -o ./archive.tar.gzip -d ./keybase_2020-02-04.gzip.gpg
-  cat "/Volumes/BACKUP/$name.gzip.gpg" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$name.gzip.gpg"
+  # gpg -o ./keybase.tar.gzip -d ./keybase_2020-02-04.gzip.gpg
 fi
 
 
--- backup/backup_lastpass.sh
+++ backup/backup_lastpass.sh
@@ -5,10 +5,14 @@ set -e
 data="$(/usr/local/bin/lpass export)"
 filename=lpass_"$(date '+%F')".gzip.gpg
 
-if [ ! -f /Volumes/BACKUP/"$filename" ]; then
-  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /Volumes/BACKUP/"$filename"
+if [ "ssh admin@192.168.31.1 stat /media/BACKUP/$filename \> /dev/null 2\>\&1" ]; then
+  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /tmp/"$filename"
+  # to download
+  # scp admin@192.168.31.1:/media/BACKUP/lpass_2020-02-10.tar.gzip.gpg ./lpass.gzip.gpg
+  scp "/tmp/$filename" admin@192.168.31.1:/media/BACKUP/
   # to decrypt
   # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
-  cat "/Volumes/BACKUP/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+  cat "/tmp/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+  rm "/tmp/$filename"
 fi
 
