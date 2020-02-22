55f20248106a90f19a078c616e9adadad004d097
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-23 00:28:22 +0200

    update backup scripts

--- backup/keybase.sh
+++ backup/keybase.sh
@@ -3,18 +3,20 @@
 set -e
 
 filename=keybase_$(date '+%F').tar.gzip.gpg
+keybase="/usr/local/bin/keybase"
+gpg="/usr/local/bin/gpg"
 
-if [ "ssh admin@192.168.31.1 stat /media/Main/$filename \> /dev/null 2\>\&1" ]; then
-  path=$($HOME/.asdf/shims/node $HOME/.dotfiles/backup/keybase.js)
-  cd "$path"
+if ! $($keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
+then
+  $HOME/.asdf/shims/node $HOME/.dotfiles/backup/keybase.js
+  cd /tmp
 
-  export LC_ALL=en_US.UTF-8 && tar -cf - ./keybase | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "./$filename"
+  export LC_ALL=en_US.UTF-8 && tar -cf - ./keybase | gzip | $gpg --encrypt -r alex.popov.tech@gmail.com > "./$filename"
   rm -rf ./keybase
   scp "./$filename" "admin@192.168.31.1:/media/Main/"
-  cat "./$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+  $keybase fs write "/keybase/public/alex_popov_tech/$filename" < "./$filename"
   rm "./$filename"
   # to download and decrypt
   # scp admin@192.168.31.1:/media/Main/keybase_2020-02-11.tar.gzip.gpg ./keybase.tar.gzip.gpg && gpg --decrypt -o keybase.tar.gzip ./keybase.tar.gzip.gpg
 fi
-
 
--- backup/lastpass.sh
+++ backup/lastpass.sh
@@ -14,8 +14,10 @@ then
   # to download
   # scp admin@192.168.31.1:/media/Main/lpass_2020-02-11.gzip.gpg ./lpass.gzip.gpg
   scp "/tmp/$filename" "admin@192.168.31.1:/media/Main/$filename"
-  # to decrypt
-  # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
+  # to download
+  # keybase fs read /keybase/public/alex_popov_tech/lpass_2020-02-11.gzip.gpg > ./lpass.gzip.gpg
   $keybase fs write "/keybase/public/alex_popov_tech/$filename" < "/tmp/$filename"
   rm "/tmp/$filename"
+  # to decrypt
+  # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
 fi
