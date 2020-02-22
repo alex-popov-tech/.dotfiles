88ee1b9ee3c8252f2d4b7788d219bfcd6b88f148
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-22 23:27:16 +0200

    update lastpass backup

--- backup/lastpass.sh
+++ backup/lastpass.sh
@@ -2,18 +2,20 @@
 
 set -e
 
+lpass="/usr/local/bin/lpass"
+keybase="/usr/local/bin/keybase"
 filename=lpass_"$(date '+%F')".gzip.gpg
 
-if [ "ssh admin@192.168.31.1 stat /media/Main/$filename \> /dev/null 2\>\&1" ]; then
-  data="$(/usr/local/bin/lpass export)"
+if ! $($keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
+then
+  data="$($lpass export --sync=now)"
 
-  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > /tmp/"$filename"
+  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "/tmp/$filename"
   # to download
   # scp admin@192.168.31.1:/media/Main/lpass_2020-02-11.gzip.gpg ./lpass.gzip.gpg
   scp "/tmp/$filename" "admin@192.168.31.1:/media/Main/$filename"
   # to decrypt
   # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
-  cat "/tmp/$filename" | /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename"
+  $keybase fs write "/keybase/public/alex_popov_tech/$filename" < "/tmp/$filename"
   rm "/tmp/$filename"
 fi
-
