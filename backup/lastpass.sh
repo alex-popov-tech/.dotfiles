30c0e12353713685d66daa50b7e2d8ca79ddb585
Alexander Popov <alex.popov.tech@gmail.com>
2020-02-23 00:35:24 +0200

    update lpass backup script

--- backup/lastpass.sh
+++ backup/lastpass.sh
@@ -4,13 +4,14 @@ set -e
 
 lpass="/usr/local/bin/lpass"
 keybase="/usr/local/bin/keybase"
+gpg="/usr/local/bin/gpg"
 filename=lpass_"$(date '+%F')".gzip.gpg
 
 if ! $($keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
 then
   data="$($lpass export --sync=now)"
 
-  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "/tmp/$filename"
+  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | $gpg --encrypt -r alex.popov.tech@gmail.com > "/tmp/$filename"
   # to download
   # scp admin@192.168.31.1:/media/Main/lpass_2020-02-11.gzip.gpg ./lpass.gzip.gpg
   scp "/tmp/$filename" "admin@192.168.31.1:/media/Main/$filename"
