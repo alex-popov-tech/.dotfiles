f67cece30d687288bcff701f313a7fc87f7fec6f
Alexander Popov <alex.popov.tech@gmail.com>
2020-08-26 09:07:46 +0300

    update finicky config

--- configs/.finicky.js
+++ configs/.finicky.js
@@ -1,17 +1,21 @@
 module.exports = {
-  defaultBrowser: "Google Chrome",
+  defaultBrowser: 'Google Chrome',
   handlers: [
     {
-      match: ['bcg-xchange.atlassian.net*', 'id.svitla.com*'],
-      browser: 'Safari'
+      match: ['bcg-xchange.atlassian.net*', 'id.svitla.com*', 'bitbucket.org/xchangeteam*'],
+      browser: 'Safari',
     },
     {
-      match: ['beta.scrumpypoker.com*', 'mlitvinova.testrail.io*'],
-      browser: 'Opera'
+      match: ['beta.scrumpypoker.com*', 'mlitvinova.testrail.io*', 'digitalproducts.atlassian.net*'],
+      browser: 'Opera',
+    },
+    {
+      match: ['cake.hr*', 'lokalise.atlassian.net*'],
+      browser: 'Firefox',
     },
     {
       match: /zoom.us\/j\//,
-      browser: "us.zoom.xos"
-    }
-  ]
+      browser: 'us.zoom.xos',
+    },
+  ],
 };
