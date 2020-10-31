99c9c1acc1a885716228b34625a5be948da74d8e
Alexander Popov <alex.popov.tech@gmail.com>
2020-10-31 19:13:17 +0200

    update backup scripts

--- backup/keybase.sh
+++ backup/keybase.sh
@@ -4,18 +4,18 @@ set -e
 
 filename=keybase_$(date '+%F').tar.gzip.gpg
 
-if ! $(keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
+if ! $(/usr/local/bin/keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
 then
   $HOME/.asdf/shims/node $HOME/.dotfiles/backup/keybase.js
   cd /tmp
 
-  export LC_ALL=en_US.UTF-8 && tar -cf - ./keybase | gzip | gpg --encrypt -r alex.popov.tech@gmail.com > "./$filename"
+  export LC_ALL=en_US.UTF-8 && tar -cf - ./keybase | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "./$filename"
   rm -rf ./keybase
   # to download and decrypt
   # scp admin@192.168.31.1:/media/Main/keybase_2020-02-11.tar.gzip.gpg ./keybase.tar.gzip.gpg && gpg --decrypt -o keybase.tar.gzip ./keybase.tar.gzip.gpg
   scp "./$filename" "router:/media/Main/"
   # keybase fs read /keybase/public/alex_popov_tech/keybase_2020-03-04.tar.gzip.gpg | gpg --decrypt | funzip > ./keybase.tar
-  keybase fs write "/keybase/public/alex_popov_tech/$filename" < "./$filename"
+  /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename" < "./$filename"
   rm "./$filename"
 fi
 
--- backup/lastpass.sh
+++ backup/lastpass.sh
@@ -4,17 +4,17 @@ set -e
 
 filename=lpass_"$(date '+%F')".gzip.gpg
 
-if ! $(keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
+if ! $(/usr/local/bin/keybase fs stat /keybase/public/alex_popov_tech/$filename &>/dev/null)
 then
-  data="$(lpass export --sync=now)"
+  data="$(/usr/local/bin/lpass export --sync=now)"
 
-  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | gpg --encrypt -r alex.popov.tech@gmail.com > "/tmp/$filename"
+  export LC_ALL=en_US.UTF-8 && echo "$data" | gzip | /usr/local/bin/gpg --encrypt -r alex.popov.tech@gmail.com > "/tmp/$filename"
   # to download and decrypt
   # scp router:/media/Main/lpass_2020-02-11.gzip.gpg ./lpass.gzip.gpg
   # gpg --decrypt ./lpass_2020-02-03.gzip.gpg | funzip
   scp "/tmp/$filename" "router:/media/Main/$filename"
   # to download and decrypt
   # keybase fs read /keybase/public/alex_popov_tech/lpass_2020-03-04.gzip.gpg | gpg --decrypt | funzip
-  keybase fs write "/keybase/public/alex_popov_tech/$filename" < "/tmp/$filename"
+  /usr/local/bin/keybase fs write "/keybase/public/alex_popov_tech/$filename" < "/tmp/$filename"
   rm "/tmp/$filename"
 fi
