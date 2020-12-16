093d279c4cbdf47c755e6425a344f5a40470cd4b
Alexander Popov <alex.popov.tech@gmail.com>
2020-12-17 00:49:54 +0200

    fix linkconfigs path

--- scripts/linkconfigs.sh
+++ scripts/linkconfigs.sh
@@ -5,4 +5,4 @@ $DPATH/scripts/lnfilepath.sh $DPATH/home/.* $HOME
 $DPATH/scripts/lnfilepath.sh $DPATH/home/.config/nvim/*.vim $HOME/.config/nvim
 $DPATH/scripts/lnfilepath.sh $DPATH/home/.config/nvim/plugins/*.vim $HOME/.config/nvim/plugins
 $DPATH/scripts/lnfilepath.sh $DPATH/home/.config/nvim/lua/*.lua $HOME/.config/nvim/lua
-$DPATH/scripts/lnfilepath.sh $DPATH/home/.config/nvim/crystalline_statusline $HOME/.config/nvim/plugged/vim-crystalline/autoload/crystalline/theme/crystalline_statusline.vim
+$DPATH/scripts/lnfilepath.sh $DPATH/home/.config/nvim/plugged/vim-crystalline/autoload/crystalline/theme/crystalline_statusline.vim $HOME/.config/nvim/plugged/vim-crystalline/autoload/crystalline/theme/crystalline_statusline.vim
