2b78b8a822d7118b161db37a14444e17f081fde4
Alexander Popov <alex.popov.tech@gmail.com>
2021-01-04 21:17:04 +0200

    add lua globals

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("globals")
 require("settings")
 require("mappings")
 require("plugins")
---
+++ home/.config/nvim/lua/globals.lua
@@ -0,0 +1,26 @@
+_G.installLsp = function()
+    -- https://github.com/mattn/vim-lsp-settings should be installed
+    -- and that set to download in single dir   vim.g.lsp_settings_servers_dir = vim.fn.stdpath("cache") .. "/lspconfig"
+    vim.cmd [[set filetype=typescript]]
+    vim.cmd [[LspInstallServer typescript-language-server]]
+    vim.cmd [[set filetype=lua]]
+    vim.cmd [[LspInstallServer sumneko-lua-language-server]]
+    vim.cmd [[set filetype=python]]
+    vim.cmd [[LspInstallServer pyls]]
+    vim.cmd [[set filetype=sh]]
+    vim.cmd [[LspInstallServer bash-language-server]]
+    vim.cmd [[set filetype=json]]
+    vim.cmd [[LspInstallServer json-languageserver]]
+    vim.cmd [[set filetype=yaml]]
+    vim.cmd [[LspInstallServer yaml-language-server]]
+    vim.cmd [[set filetype="*"]]
+    vim.cmd [[LspInstallServer efm-langserver]]
+end
+
+_G.printt = function(tbl)
+    print(vim.inspect(tbl))
+end
+
+_G.fmt = function()
+    vim.lsp.buf.formatting_sync(nil, 500)
+end
