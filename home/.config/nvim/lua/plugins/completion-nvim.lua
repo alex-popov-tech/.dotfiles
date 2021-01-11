5c57c1d38c777763dd1c03a961089eeafd4f5cd6
Alexander Popov <alex.popov.tech@gmail.com>
2021-01-12 01:11:24 +0200

    update completion.nvim settings

--- home/.config/nvim/lua/plugins/completion-nvim.lua
+++ home/.config/nvim/lua/plugins/completion-nvim.lua
@@ -1,11 +1,4 @@
 return function()
-    -- vim.g.completion_chain_complete_list = {
-    --     default = {
-    --         {complete_items = {"lsp", "tabnine", "buffers" }},
-    --         {mode = "<c-n>"},
-    --         {mode = "<c-p>"}
-    --     },
-    -- }
     vim.g.completion_items_priority = {
         Field = 10,
         Method = 10,
@@ -22,8 +15,30 @@ return function()
         File = 2
     }
     vim.g.completion_sorting = "length"
-    vim.g.completion_auto_change_source = 1
     vim.g.completion_matching_strategy_list = {"exact", "substring", "fuzzy", "all"}
     vim.g.completion_matching_smart_case = 1
     vim.g.completion_tabnine_sort_by_details = 1
+    vim.g.completion_trigger_character = {".", "::"}
+    vim.g.completion_trigger_keyword_length = 2
+    vim.g.completion_timer_cycle = 50
+    vim.g.completion_customize_lsp_label = {
+        Function = " [function]",
+        Method = " [method]",
+        Reference = " [refrence]",
+        Enum = " [enum]",
+        Field = "ﰠ [field]",
+        Keyword = " [key]",
+        Variable = " [variable]",
+        Folder = " [folder]",
+        Snippet = " [snippet]",
+        Operator = " [operator]",
+        Module = " [module]",
+        Text = "ﮜ [text]",
+        Class = " [class]",
+        Interface = " [interface]",
+        File = " [file]"
+    }
+    vim.g.completion_enable_snippet = 'snippets.nvim'
+    createMapping("i", "<c-k>", "<cmd>lua return require'snippets'.expand_or_advance(1)")
+    createMapping("i", "<c-j>", "<cmd>lua return require'snippets'.advance_snippet(-1)")
 end
