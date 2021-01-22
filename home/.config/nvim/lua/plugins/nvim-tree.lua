e39edaf328aac081b6cde12466b12a22218b1f05
Alexander Popov <alex.popov.tech@gmail.com>
2021-01-22 16:53:07 +0200

    update nvim filetree configs

--- home/.config/nvim/lua/plugins/nvim-tree.lua
+++ home/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,15 +1,15 @@
 return function()
     map("n", "<c-f>", ":NvimTreeFindFile<cr>")
-    g.lua_tree_side = "left"
-    g.lua_tree_width = 30
-    g.lua_tree_auto_close = 1
-    g.lua_tree_quit_on_open = 1
-    g.lua_tree_follow = 1
-    g.lua_tree_indent_markers = 1
-    g.lua_tree_git_hl = 1
-    g.lua_tree_allow_resize = 1
-    g.lua_tree_show_icons = {git = 1, folders = 1, files = 1}
-    g.lua_tree_bindings = {
+    g.nvim_tree_side = "left"
+    g.nvim_tree_width = 30
+    g.nvim_tree_auto_close = 1
+    g.nvim_tree_quit_on_open = 1
+    g.nvim_tree_follow = 1
+    g.nvim_tree_indent_markers = 1
+    g.nvim_tree_git_hl = 1
+    g.nvim_tree_allow_resize = 1
+    g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
+    g.nvim_tree_bindings = {
         edit = {"<CR>", "l"},
         edit_vsplit = "<C-v>",
         edit_split = "<C-x>",
@@ -22,7 +22,7 @@ return function()
         copy = "y",
         paste = "p"
     }
-    g.lua_tree_icons = {
+    g.nvim_tree_icons = {
         default = "",
         symlink = "",
         git = {
