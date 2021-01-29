94da03a33be629ada58642e6a8ab5747a7f8e03e
Alexander Popov <alex.popov.tech@gmail.com>
2021-01-29 10:44:24 +0200

    move efm

--- home/.config/nvim/lua/lsp/servers/efm.lua
+++ home/.config/nvim/lua/lsp/servers/efm.lua
@@ -1,17 +1,43 @@
 return function(config)
-	local bin_path = vim.fn.stdpath("cache") .. "/lspconfig/efm-langserver/efm-langserver"
-    config.efm.setup {
-    default_config = {
-      cmd = {
-        bin_path,
-        "-c",
-        [[/Users/alexanderpopov/.config/efm-langserver/config.yaml]]
-      }
-    },
-    filetypes = {
-      "javascript",
-      "typescript",
-      "lua"
+    local util = require "lspconfig/util"
+    local eslint_d = {
+        lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+        lintStdin = true,
+        lintFormats = {"%f:%l:%c: %m"},
+        lintIgnoreExitCode = true,
+        formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+        formatStdin = true
     }
-  }
+    local luaFormat = {
+        formatCommand = "luafmt --stdin",
+        formatStdin = true
+    }
+    local languages = {
+        typescript = {eslint_d},
+        javascript = {eslint_d},
+        typescriptreact = {eslint_d},
+        javascriptreact = {eslint_d},
+        lua = {luaFormat}
+    }
+    local bin_path = vim.fn.stdpath("cache") .. "/lspconfig/efm-langserver/efm-langserver"
+    config.efm.setup {
+        default_config = {
+            cmd = bin_path
+        },
+        root_dir = util.root_pattern(".git", vim.fn.getcwd()),
+        init_options = {
+            documentFormatting = true,
+            codeAction = true
+        },
+        settings = {
+            languages = languages
+        },
+        filetypes = {
+            "javascript",
+            "typescript",
+            "typescriptreact",
+            "javascriptreact",
+            "lua"
+        }
+    }
 end
