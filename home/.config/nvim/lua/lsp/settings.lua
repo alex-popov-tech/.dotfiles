e22efcf863305cfc774c3eee244686d8da8dcd53
Alexander Popov <alex.popov.tech@gmail.com>
2021-02-26 17:59:32 +0200

    update completion labels

--- home/.config/nvim/lua/lsp/settings.lua
+++ home/.config/nvim/lua/lsp/settings.lua
@@ -4,4 +4,31 @@ return function()
     vim.fn.sign_define("LspDiagnosticsSignWarning", {numhl = "LspDiagnosticsSignWarning"})
     vim.fn.sign_define("LspDiagnosticsSignInformation", {numhl = "LspDiagnosticsSignInformation"})
     vim.fn.sign_define("LspDiagnosticsSignHint", {numhl = "LspDiagnosticsSignHint"})
+    require "vim.lsp.protocol".CompletionItemKind = {
+      "ﮜ [text]",
+      " [method]",
+      " [function]",
+      " [constructor]",
+      "ﰠ [field]",
+      " [variable]",
+      " [class]",
+      " [interface]",
+      " [module]",
+      " [property]",
+      " [unit]",
+      " [value]",
+      " [enum]",
+      " [key]",
+      " [snippet]",
+      " [color]",
+      " [file]",
+      " [reference]",
+      " [folder]",
+      " [enum member]",
+      " [constant]",
+      " [struct]",
+      "⌘ [event]",
+      " [operator]",
+      "⌂ [type]"
+    }
 end
