9b7c3147761fa79ccd082d6cf1d482312a6e90df
Alexander Popov <alex.popov.tech@gmail.com>
2021-03-06 22:43:31 +0200

    sonokai and ui updates

--- home/.config/nvim/lua/plugins/sonokai.lua
+++ home/.config/nvim/lua/plugins/sonokai.lua
@@ -1,6 +1,7 @@
 return function()
-    vim.g.sonokai_style = "shusia"
-    vim.g.sonokai_enable_italic = 1
-    vim.g.sonokai_better_performance = 1
+  -- semehow its not working here, so i move that to ui.lua
+    -- vim.g.sonokai_style = "shusia"
+    -- vim.g.sonokai_enable_italic = 1
+    -- vim.g.sonokai_better_performance = 1
     -- vim.g.sonokai_transparent_background = 1
 end
--- home/.config/nvim/lua/ui.lua
+++ home/.config/nvim/lua/ui.lua
@@ -1,20 +1,18 @@
 g.nvcode_termcolors = 256 -- true colors
+
+g.sonokai_style = "shusia"
+g.sonokai_enable_italic = 1
 cmd("colorscheme sonokai")
 
--- highlight modified buffers for tabline
-hi("BufferCurrentMod", {guifg="#00afff"})
-hi("BufferVisibleMod", {guifg="#00afff"})
-hi("BufferInactiveMod", {guifg="#00afff"})
+-- custom highlights for fugitive
+hi("DiffAdd", {guifg = "#5fd700"})
+hi("DiffText", {guifg = "#00afff"})
+hi("DiffChange", {guifg = "#ffff00"})
+hi("DiffDelete", {guifg = "#ff0000"})
 
 -- custom highlights for fugitive
-hi("DiffAdd", {guifg="#5fd700"})
-hi("DiffText", {guifg="#00afff"})
-hi("DiffChange", {guifg="#ffff00"})
-hi("DiffDelete", {guifg="#ff0000"})
-
--- custom highlights for fugitive
-hi("NvimTreeGitNew", {guifg="#5fd700"})
-hi("NvimTreeGitDirty", {guifg="#ffff00"})
-hi("NvimTreeGitRenamed", {guifg="#ffff00"})
-hi("NvimTreeGitMerge", {guifg="#ff0000"})
-hi("NvimTreeGitStaged", {guifg="#00afff"})
+hi("NvimTreeGitNew", {guifg = "#5fd700"})
+hi("NvimTreeGitDirty", {guifg = "#ffff00"})
+hi("NvimTreeGitRenamed", {guifg = "#ffff00"})
+hi("NvimTreeGitMerge", {guifg = "#ff0000"})
+hi("NvimTreeGitStaged", {guifg = "#00afff"})
