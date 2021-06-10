989fb013963b46acd7b475f13157289afb16b1cd
Alexander Popov <alex.popov.tech@gmail.com>
2021-06-10 18:41:20 +0300

    update floaterm ui

--- home/.config/nvim/lua/plugins/vim-floaterm.lua
+++ home/.config/nvim/lua/plugins/vim-floaterm.lua
@@ -1,6 +1,8 @@
 return function()
-    g.floaterm_width = 0.9
-    g.floaterm_height = 0.9
+    g.floaterm_width = 0.99
+    g.floaterm_height = 0.99
+    g.floaterm_title = ""
     map("n", "<F11>", ":FloatermToggle<cr>")
     map("t", "<F11>", "<c-\\><c-n>:FloatermToggle<cr>")
+    hi("FloatermBorder", {guibg = "none", guifg = "none"})
 end
