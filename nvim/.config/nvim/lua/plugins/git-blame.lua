30a0ba2853acd1775ce006be7015d1d9a1d9b9fa
Alexander Popov <alex.popov.tech@gmail.com>
2021-10-11 17:20:29 +0300

    delete lspsaga & git-blame plugins

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    vim.g.gitblame_enabled = 0
-    require "timer".add(
-        function()
-            require("gitblame").show_blame_info()
-            return 5000
-        end
-    )
-end
