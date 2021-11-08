044bafc2c4827d358e8908b64b4ecbbd250cce12
Alexander Popov <alex.popov.tech@gmail.com>
2021-11-08 20:19:13 +0200

    update backup scripts

--- .scripts/backup/mailOnErr.sh
+++ .scripts/backup/mailOnErr.sh
@@ -8,7 +8,7 @@ while  true ; do
   $1 >/dev/null 2>$tmp;
   if [ -s $tmp ]
   then
-    mail -s BACKUP_ERROR alex.popov.tech@gmail.com < $tmp
+    mail -s BACKUP_INFO alex.popov.tech@gmail.com < $tmp
     rm $tmp
     sleep 3600
   else
