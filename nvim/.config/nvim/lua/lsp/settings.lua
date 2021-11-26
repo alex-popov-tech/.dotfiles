74e1d876b5b32680ef6b47ed560fc776c49c6247
Alexander Popov <alex.popov.tech@gmail.com>
2021-11-26 23:49:20 +0200

    update diagnostic settings

--- nvim/.config/nvim/lua/lsp/settings.lua
+++ nvim/.config/nvim/lua/lsp/settings.lua
@@ -1,13 +1,14 @@
 return function()
     -- define line number hl for lines with Lsp errors
-    hi("DiagnosticVirtualTextWarn", {guifg = "#e7c664"})
-    hi("DiagnosticVirtualTextError", {guifg = "#fc5d7c"})
     vim.cmd [[sign define DiagnosticSignWarn text= texthl= numhl=DiagnosticSignWarn linehl=]]
     vim.cmd [[sign define DiagnosticSignError text= texthl= numhl=DiagnosticSignError linehl=]]
 
-
     -- Disable the default signs handler
-    vim.diagnostic.config({signs = false})
+    vim.diagnostic.config(
+        {
+            signs = false
+        }
+    )
 
     -- Create a namespace. This won't be used to add any diagnostics,
     -- only to display them.
@@ -38,8 +39,9 @@ return function()
             filtered_diagnostics,
             {
                 virtual_text = false,
-                underline = false,
-                signs = true
+                underline = true,
+                signs = true,
+                severity_sort = true,
             }
         )
     end
@@ -48,4 +50,45 @@ return function()
         orig_show(namespace, bufnr, ...)
         set_signs(bufnr)
     end
+
+    vim.lsp.buf.rename = {
+      float = function()
+        local currName = vim.fn.expand("<cword>")
+        local tshl = require("nvim-treesitter-playground.hl-info").get_treesitter_hl()
+        if tshl and #tshl > 0 then
+          local ind = tshl[#tshl]:match("^.*()%*%*.*%*%*")
+          tshl = tshl[#tshl]:sub(ind + 2, -3)
+        end
+
+        local win = require('plenary.popup').create(currName, {
+          title = "New Name",
+          style = "minimal",
+          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+          relative = "cursor",
+          borderhighlight = "FloatBorder",
+          titlehighlight = "Title",
+          highlight = tshl,
+          focusable = true,
+          width = 25,
+          height = 1,
+          line = "cursor+2",
+          col = "cursor-1"
+        })
+
+        local map_opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(0, "i", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+        vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+        vim.api.nvim_buf_set_keymap(0, "i", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName..","..win.."')<CR>", map_opts)
+        vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName..","..win.."')<CR>", map_opts)
+      end,
+      apply = function(curr, win)
+        local newName = vim.trim(vim.fn.getline('.'))
+        vim.api.nvim_win_close(win, true)
+        if #newName > 0 and newName ~= curr then
+          local params = vim.lsp.util.make_position_params()
+          params.newName = newName
+          vim.lsp.buf_request(0, "textDocument/rename", params)
+        end
+      end
+    }
 end
