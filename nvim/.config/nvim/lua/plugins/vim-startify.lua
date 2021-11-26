ca4f35cb267092db7fcb6763c6feedfae3601e2a
Alexander Popov <alex.popov.tech@gmail.com>
2021-11-27 00:19:38 +0200

    fix startify config

--- nvim/.config/nvim/lua/plugins/vim-startify.lua
+++ nvim/.config/nvim/lua/plugins/vim-startify.lua
@@ -5,5 +5,5 @@ return function()
     -- do not show 'edit' and 'quit' options
     g.startify_enable_special = 1
     -- start from 1 when choosing
-    g.startify_custom_indices = {"a", "r", "s", "t", "n", "e", "o", "i", 1, 2, 3, 4, 7, 8, 9, 0}
+    g.startify_custom_indices = {"a", "r", "e", "t", "o", "s", "i", "n"}
 end
