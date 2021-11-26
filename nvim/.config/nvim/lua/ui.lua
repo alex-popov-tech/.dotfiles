4838b67da55e4f8b090dc5371fb13f12e35eeee0
Alexander Popov <alex.popov.tech@gmail.com>
2021-11-27 00:13:51 +0200

    adjust ui

--- nvim/.config/nvim/lua/ui.lua
+++ nvim/.config/nvim/lua/ui.lua
@@ -7,7 +7,7 @@ cmd("colorscheme sonokai")
 -- highlight split separators
 hi("VertSplit", {guibg = "none", guifg = "#78dce8"})
 hi("StatusLineNC", {guibg = "none", guifg = "#78dce8"})
-hi("StatusLine", {guibg = "none", guifg = "none"})
+hi("StatusLine", {guibg = "none", guifg = "#78dce8"})
 -- custom highlights for fugitive
 hi("DiffAdd", {guifg = "#5fd700"})
 hi("DiffText", {guifg = "#00afff"})
@@ -20,3 +20,11 @@ hi("NvimTreeGitDirty", {guifg = "#ffff00"})
 hi("NvimTreeGitRenamed", {guifg = "#ffff00"})
 hi("NvimTreeGitMerge", {guifg = "#ff0000"})
 hi("NvimTreeGitStaged", {guifg = "#00afff"})
+
+-- hl groups for diagnostic & float
+hi("DiagnosticVirtualTextWarn", {guifg = "#e7c664", guibg = "none"})
+hi("DiagnosticVirtualTextError", {guifg = "#fc5d7c", guibg = "none"})
+hi("DiagnosticFloatingError", {guifg = "#fc5d7c", guibg = "none"})
+hi("DiagnosticFloatingWarn", {guifg = "#e7c664", guibg = "none"})
+hi("NormalFloat", {guibg = "none"})
+
