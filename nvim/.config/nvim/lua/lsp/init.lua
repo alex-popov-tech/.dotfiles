9ba61020b62ab31a4db2958cfb720ca7a1b48115
Alexander Popov <alex.popov.tech@gmail.com>
2022-01-05 00:16:01 +0200

    update lsp init

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,7 +1,7 @@
+require("lsp.settings")()
 local lsp_installer = require("nvim-lsp-installer")
 
-local servers = { "jsonls", "tsserver", "jsonls", "sumneko_lua", "terraformls"}
--- local servers = { "efm", "jsonls", "tsserver", "jsonls", "sumneko_lua", "terraformls"}
+local servers = { "sumneko_lua", "jsonls", "tsserver" }
 
 _G.installLspServers = function()
     cmd("LspUninstallAll")
@@ -14,14 +14,10 @@ end
 
 lsp_installer.on_server_ready(
     function(server)
-        -- todo https://github.com/williamboman/nvim-lsp-installer/issues/63
-        if not includes(servers, server.name) then
-            return
-        end
-
         local general_on_attach = require("lsp.on_attach")
 
         local serverConfig = require("lsp.servers." .. server.name)(general_on_attach)
+
         serverConfig.flags = {debounce_text_changes = 100, lintDebounce = 200}
         serverConfig.capabilities =
             require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -29,6 +25,5 @@ lsp_installer.on_server_ready(
         server:setup(serverConfig)
         vim.cmd [[ do User LspAttachBuffers ]]
 
-        require("lsp.settings")()
     end
 )
