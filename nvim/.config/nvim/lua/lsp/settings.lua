f95cb6369eb07b82e9a0dec4f3ac97c4b0aa9d0a
Alexander Popov <alex.popov.tech@gmail.com>
2022-01-05 00:15:40 +0200

    update lsp settings

--- nvim/.config/nvim/lua/lsp/settings.lua
+++ nvim/.config/nvim/lua/lsp/settings.lua
@@ -3,53 +3,26 @@ return function()
     vim.cmd [[sign define DiagnosticSignWarn text= texthl= numhl=DiagnosticSignWarn linehl=]]
     vim.cmd [[sign define DiagnosticSignError text= texthl= numhl=DiagnosticSignError linehl=]]
 
-    -- Disable the default signs handler
+    -- set global diagnostic config
     vim.diagnostic.config(
         {
-            signs = false
+            signs = true,
+            underline = true,
+            virtual_text = { prefix = '<' },
+            float = { scope = "line", border = "rounded", focusable = false },
+            severity_sort = true,
         }
     )
 
-    -- Create a namespace. This won't be used to add any diagnostics,
-    -- only to display them.
-    local ns = vim.api.nvim_create_namespace("my_namespace")
-
-    -- Create a reference to the original function
-    local orig_show = vim.diagnostic.show
-
-    local function set_signs(bufnr)
-        -- Get all diagnostics from the current buffer
-        local diagnostics = vim.diagnostic.get(bufnr)
-
-        -- Find the "worst" diagnostic per line
-        local max_severity_per_line = {}
-        for _, d in pairs(diagnostics) do
-            local m = max_severity_per_line[d.lnum]
-            if not m or d.severity < m.severity then
-                max_severity_per_line[d.lnum] = d
-            end
-        end
-
-        -- Show the filtered diagnostics using the custom namespace. Use the
-        -- reference to the original function to avoid a loop.
-        local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
-        orig_show(
-            ns,
-            bufnr,
-            filtered_diagnostics,
-            {
-                virtual_text = false,
-                underline = true,
-                signs = true,
-                severity_sort = true,
-            }
-        )
-    end
-
-    function vim.diagnostic.show(namespace, bufnr, ...)
-        orig_show(namespace, bufnr, ...)
-        set_signs(bufnr)
-    end
+    -- add borders to some floating things
+    vim.lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+      focusable = false,
+    })
+    vim.lsp.handlers["textDocument/signatureHelp"] = lsp.with(vim.lsp.handlers.signature_help, {
+      border = "rounded",
+      focusable = false,
+    })
 
     vim.lsp.buf.rename = {
       float = function()
