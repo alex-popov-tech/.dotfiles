5fc4a903ec2439ad3e17baa54c9197a5d7451366
Alexander Popov <alex.popov.tech@gmail.com>
2022-01-11 01:09:46 +0200

    upgrade color

--- .scripts/test_color.sh
+++ .scripts/test_color.sh
@@ -98,3 +98,16 @@ for i in `seq 255 -1 128`; do
     echo -n " "
 done
 resetOutput
+awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+	r = 255-(colnum*255/76);
+	g = (colnum*510/76);
+	b = (colnum*255/76);
+	if (g>255) g = 510-g;
+	printf "\033[48;2;%d;%d;%dm", r,g,b;
+	printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+	printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+}'
