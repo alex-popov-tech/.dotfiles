4074ce8581a9151916f9dc56a62cd281e4b7de23
Alexander Popov <alex.popov.tech@gmail.com>
2022-01-17 12:58:25 +0200

    delete comments

--- nvim/.config/nvim/lua/lsp/servers/customs.lua
+++ nvim/.config/nvim/lua/lsp/servers/customs.lua
@@ -5,26 +5,6 @@ local server = require 'nvim-lsp-installer.server'
 local path = require 'nvim-lsp-installer.path'
 local npm = require 'nvim-lsp-installer.installers.npm'
 
--- local server_name = "emmet-language-server"
-
--- configs[server_name] = {
---     default_config = {
---       filetypes = {
---           'html', 'typescriptreact', 'javascriptreact', 'javascript',
---           'typescript', 'javascript.jsx', 'typescript.tsx', 'css'
---       },
---       root_dir = function() return lspconfig.util.find_git_ancestor(vim.fn.getcwd()) or lspconfig.util.find_package_json_ancestor(vim.fn.getcwd()) end,
---     };
--- }
-
--- servers.register(server.Server:new {
---     name = server_name,
---     root_dir = server.get_server_root_path(server_name),
---     installer = npm.packages{'@kozer/emmet-language-server'},
---     default_options = {
---         cmd = {path.concat{ server.get_server_root_path(server_name),"node_modules",".bin", "emmet-language-server" }, "--stdio" },
---     },
--- })
 local custom_servers = {
     {
         server_name = 'ls_emmet',
@@ -70,42 +50,3 @@ for _, config in pairs(custom_servers) do
   configs[config.server_name] = config.lspconfig
   servers.register(config.installer_server)
 end
-
--- local server_name = 'ls_emmet'
-
--- configs[server_name] = {
---     default_config = {
---         filetypes = {
---             'html',
---             'typescriptreact',
---             'javascriptreact',
---             'javascript',
---             'typescript',
---             'javascript.jsx',
---             'typescript.tsx',
---             'css'
---         },
---         root_dir = function()
---             return lspconfig.util.find_git_ancestor(vim.fn.getcwd()) or
---                        lspconfig.util
---                            .find_package_json_ancestor(vim.fn.getcwd())
---         end
---     }
--- }
-
--- servers.register(server.Server:new{
---     name = server_name,
---     root_dir = server.get_server_root_path(server_name),
---     installer = npm.packages {server_name},
---     default_options = {
---         cmd = {
---             path.concat {
---                 server.get_server_root_path(server_name),
---                 'node_modules',
---                 '.bin',
---                 'ls_emmet'
---             },
---             '--stdio'
---         }
---     }
--- })
