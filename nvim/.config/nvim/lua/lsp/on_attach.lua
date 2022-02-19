f5e1e0f65da1cb37b858110899c6cbf5b493713c
Alexander Popov <alex.popov.tech@gmail.com>
2022-02-19 19:04:22 +0200

    update common on_attach

--- nvim/.config/nvim/lua/lsp/on_attach.lua
+++ nvim/.config/nvim/lua/lsp/on_attach.lua
@@ -2,13 +2,13 @@ return function(client, bufnr)
     local options = {noremap = true, silent = true}
 
     if client.resolved_capabilities.hover then
-        map("n", "K", function() vim.lsp.buf.hover() end, options)
+        map('n', 'K', function() vim.lsp.buf.hover() end, options)
     end
     if client.resolved_capabilities.signature_help then
-        map("i", "<c-k>", function() vim.lsp.buf.signature_help() end, options)
+        map('i', '<c-k>', function() vim.lsp.buf.signature_help() end, options)
     end
     if client.resolved_capabilities.find_references then
-        map("n", "'gr", function()
+        map('n', '\'gr', function()
             require'telescope.builtin'.lsp_references({
                 layout_strategy = 'vertical',
                 layout_config = {width = 0.9, height = 0.9}
@@ -16,37 +16,35 @@ return function(client, bufnr)
         end, options)
     end
     if client.resolved_capabilities.goto_definition then
-        map("n", "'gd", function() vim.lsp.buf.definition() end, options)
+        map('n', '\'gd', function() vim.lsp.buf.definition() end, options)
     end
     if client.resolved_capabilities.rename then
-        map("n", "'rn", function() vim.lsp.buf.rename.float() end, options)
+        map('n', '\'rn', function() vim.lsp.buf.rename() end, options)
+        --  map('n', '\'rn', function() vim.lsp.buf.rename.float() end, options)
     end
 
-    map("n", "[d",
+    map('n', '[d',
         function() vim.diagnostic.goto_prev({float = {border = 'single'}}) end,
         options)
-    map("n", "]d",
+    map('n', ']d',
         function() vim.diagnostic.goto_next({float = {border = 'single'}}) end,
         options)
-    map("n", "'gi", function() vim.lsp.buf.implementation() end, options)
-    map("n", "'a", "<cmd>CodeActionMenu<CR>", options)
-    map("n", "'d", function()
+    map('n', "'gi", function() vim.lsp.buf.implementation() end, options)
+    map('n', "'a", '<cmd>CodeActionMenu<CR>', options)
+    map('n', "'d", function()
         vim.diagnostic.open_float(0, {
-            scope = "line",
-            border = "rounded",
+            scope = 'line',
+            border = 'rounded',
             focusable = false
         })
     end, options)
-    map("n", "'D", "<cmd>TroubleToggle<CR>", options)
+    map('n', '\'D', '<cmd>TroubleToggle<CR>', options)
 
-    function _G.fmt()
-        if ft() == "typescript" then
-            require"nvim-lsp-ts-utils".organize_imports_sync()
+    map('n', '<leader>f', function()
+        if ft() == 'typescript' then
+            require'nvim-lsp-ts-utils'.organize_imports_sync()
         end
 
         vim.lsp.buf.formatting_seq_sync()
-        -- vim.cmd("w | :e | TSBufEnable highlight | :e")
-        -- vim.cmd("TSBufEnable highlight")
-    end
-    map("n", "<leader>f", function() fmt() end, options)
+    end, options)
 end
