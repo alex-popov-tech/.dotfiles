7434fcac633aac0b40820ac28b8da09d8a9a3be9
Alexander Popov <alex.popov.tech@gmail.com>
2022-02-19 19:02:41 +0200

    update nvim-lightspeed config

--- nvim/.config/nvim/lua/plugins/lightspeed-nvim.lua
+++ nvim/.config/nvim/lua/plugins/lightspeed-nvim.lua
@@ -1,1 +1,7 @@
-return function() require("lightspeed").setup {ignore_case = true} end
+return function()
+    require('lightspeed').setup {
+        ignore_case = true,
+        jump_to_unique_chars = {safety_timeout = 400}
+    }
+    cmd('nmap s <Plug>Lightspeed_omni_s')
+end
