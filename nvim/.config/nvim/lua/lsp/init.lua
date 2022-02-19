7bb96ed81f317ca1feae0e8f4526c26f67c7b999
Alexander Popov <alex.popov.tech@gmail.com>
2022-02-19 21:44:51 +0200

    remove extra null-ls related code

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,10 +1,6 @@
 require("lsp.settings")()
 local lsp_installer = require("nvim-lsp-installer")
 require("lsp.servers.customs")
-local null_ls = require("null-ls")
-local diagnostics = null_ls.builtins.diagnostics
-local formatting = null_ls.builtins.formatting
-local code_actions = null_ls.builtins.code_actions
 
 local general_on_attach = require("lsp.on_attach")
 local servers = {"sumneko_lua", "jsonls", "tsserver", "ls_emmet"}
@@ -29,25 +25,3 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd [[ do User LspAttachBuffers ]]
 
 end)
-
-null_ls.setup({
-    sources = {
-        formatting.prettierd,
-        formatting.codespell,
-        formatting.fixjson,
-        formatting.lua_format,
-        diagnostics.eslint_d.with({timeout = 5000}),
-        diagnostics.codespell,
-        diagnostics.yamllint,
-        diagnostics.write_good.with({
-          extra_filetypes = { "gitcommit" },
-        }),
-        diagnostics.markdownlint,
-        diagnostics.proselint.with({
-          extra_filetypes = { "gitcommit", "markdown" },
-        }),
-        code_actions.eslint_d,
-        code_actions.proselint,
-    },
-    on_attach = general_on_attach
-})
