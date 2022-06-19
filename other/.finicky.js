4ffbbaecdcc85910821daddccc204302a9ea8bea
Alexander Popov <alex.popov.tech@gmail.com>
2022-06-20 00:33:50 +0300

    finicky: remove old profiles

--- other/.finicky.js
+++ other/.finicky.js
@@ -1,36 +1,29 @@
 const browsers = {
   personal: {
     name: 'Brave Browser',
-    // profile: 'Default'
-    // name: 'org.mozilla.firefoxdeveloperedition-personal',
-    // appType: 'bundleId',
-    // openInBackground: false
-  },
-  personal_work: {
-    name: 'Firefox Developer Edition',
   },
   lokalise: {
     name: 'Brave Browser',
     profile: 'Profile 1'
-    // name: 'org.mozilla.firefoxdeveloperedition-lokalise',
-    // appType: 'bundleId',
-    // openInBackground: false
-  },
-  waverley: {
-    name: 'Brave Browser',
-    profile: 'Profile 2'
-    // name: 'org.mozilla.firefoxdeveloperedition-waverley',
-    // appType: 'bundleId',
-    // openInBackground: false
-  },
-  dataart: {
-    name: 'Brave Browser',
-    profile: 'Profile 3'
-    // name: 'org.mozilla.firefoxdeveloperedition-waverley',
-    // appType: 'bundleId',
-    // openInBackground: false
   },
 };
 
 module.exports = {
+  defaultBrowser: browsers.personal,
+  handlers: [
+    {
+      match: /zoom.us\/j\//,
+      browser: 'us.zoom.xos',
+    },
+    {
+      match: [
+        '*lokalise*',
+        'https://meet.google.com/isy-mnwr-mas*',
+        'https://meet.google.com/pfu-saon-rqy',
+        '*greenhouse*',
+        '*papayaglobal*',
+      ],
+      browser: browsers.lokalise
+    },
+  ],
 };
