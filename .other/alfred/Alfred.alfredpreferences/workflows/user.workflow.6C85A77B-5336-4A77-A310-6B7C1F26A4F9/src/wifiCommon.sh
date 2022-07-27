d84e939a8ffee59cccac4948d2517543fe0b8257
Alexander Popov <alex.popov.tech@gmail.com>
2022-07-27 18:26:24 +0300

    alfred: remove wifi/ble/etc plugin

--- .other/alfred/Alfred.alfredpreferences/workflows/user.workflow.6C85A77B-5336-4A77-A310-6B7C1F26A4F9/src/wifiCommon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. src/helpers.sh
-
-LIST=$(networksetup -listallhardwareports)
-INTERFACE=$(getWifiInterface "$LIST")
-NAME=$(getWifiName "$LIST")
-
-AIRPORT="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
