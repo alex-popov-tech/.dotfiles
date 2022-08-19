afc5c61a1faa3cc7384daa7fbe375d7ebf6424db
Alexander Popov <alex.popov.tech@gmail.com>
2022-08-19 23:11:23 +0300

    nvim: update compiled

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,6 @@
 require('impatient')
 require("init")
+
+-- local Path = require "plenary.path"
+-- local absolute_cwd = Path:new(vim.loop.cwd()):absolute()
+-- pcall(vim.cmd, 'source ' .. absolute_cwd .. '/' .. '.conf.lua')
