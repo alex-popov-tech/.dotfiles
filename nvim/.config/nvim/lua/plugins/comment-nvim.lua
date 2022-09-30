93879bdc2bbdb64c1bbe6afae549800f48d1fd97
Alexander Popov <alex.popov.tech@gmail.com>
2022-10-01 02:24:34 +0300

    nvim: update comment.nvim plugin config

--- nvim/.config/nvim/lua/plugins/comment-nvim.lua
+++ nvim/.config/nvim/lua/plugins/comment-nvim.lua
@@ -1,26 +1,27 @@
 return function()
-  require('Comment').setup({
-    pre_hook = function(ctx)
-      -- Only calculate commentstring for tsx filetypes
-      if vim.bo.filetype == 'typescriptreact' then
-        local U = require('Comment.utils')
+	require('Comment').setup({
+		toggler = { line = '<leader>cc' }, opleader = { line = '<leader>c' },
+		pre_hook = function(ctx)
+			-- Only calculate commentstring for tsx filetypes
+			if vim.bo.filetype == 'typescriptreact' then
+				local U = require('Comment.utils')
 
-        -- Determine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
+				-- Determine whether to use linewise or blockwise commentstring
+				local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
 
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.blockwise then
-          location = require('ts_context_commentstring.utils').get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
+				-- Determine the location where to calculate commentstring from
+				local location = nil
+				if ctx.ctype == U.ctype.blockwise then
+					location = require('ts_context_commentstring.utils').get_cursor_location()
+				elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+					location = require('ts_context_commentstring.utils').get_visual_start_location()
+				end
 
-        return require('ts_context_commentstring.internal').calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end
-    end,
-  })
+				return require('ts_context_commentstring.internal').calculate_commentstring({
+					key = type,
+					location = location,
+				})
+			end
+		end,
+	})
 end
