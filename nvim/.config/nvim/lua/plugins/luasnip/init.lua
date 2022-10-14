2ae379b462e367938a1472fd096e476d51fd5b60
Alexander Popov <alex.popov.tech@gmail.com>
2022-10-15 00:53:19 +0300

    nvim: apply more formatting

--- nvim/.config/nvim/lua/plugins/luasnip/init.lua
+++ nvim/.config/nvim/lua/plugins/luasnip/init.lua
@@ -1,5 +1,5 @@
 return function()
-    vim.cmd [[
+  vim.cmd [[
     imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
     " -1 for jumping backwards.
     inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
@@ -12,9 +12,9 @@ return function()
     smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
   ]]
 
-    require('luasnip.loaders.from_vscode').lazy_load()
-    require('plugins.luasnip.all')
-    require('plugins.luasnip.javascript')
-    require('plugins.luasnip.go')
-    require('plugins.luasnip.octo')
+  require('luasnip.loaders.from_vscode').lazy_load()
+  require('plugins.luasnip.all')
+  require('plugins.luasnip.javascript')
+  require('plugins.luasnip.go')
+  require('plugins.luasnip.octo')
 end
