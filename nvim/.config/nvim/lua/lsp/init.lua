4a565028cca078298a6092391c264a41a670cdbd
Alexander Popov <alex.popov.tech@gmail.com>
2022-10-17 01:04:57 +0300

    nvim: update with cmp deprecation

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -13,9 +13,8 @@ M.setup = function()
       general_on_attach)
 
     serverConfig.flags = { debounce_text_changes = 100, lintDebounce = 200 }
-    serverConfig.capabilities = serverConfig.capabilities or require('cmp_nvim_lsp').update_capabilities(
-      vim.lsp.protocol
-      .make_client_capabilities())
+    serverConfig.capabilities = serverConfig.capabilities or require('cmp_nvim_lsp').default_capabilities()
+
     require('lspconfig')[serverName].setup(serverConfig)
   end
 
--- nvim/.config/nvim/lua/lsp/servers/gopls.lua
+++ nvim/.config/nvim/lua/lsp/servers/gopls.lua
@@ -1,6 +1,6 @@
 return function(on_attach)
 
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) --nvim-cmp
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 
