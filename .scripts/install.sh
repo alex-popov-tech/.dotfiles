411d14f48f165f07298339268ae243cfe4e707e2
Alexander Popov <alex.popov.tech@gmail.com>
2022-10-17 01:02:52 +0300

    install: fix homebrew installation

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -107,7 +107,7 @@ function software() {
   echo "|        Installing Homebrew        |"
   echo "+-----------------------------------+"
   echo
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   echo
   echo "+-------------------------------+"
