37f3a9636ef7197026b560ffd611401cce57af79
Olexander Popov <alex.popov.tech@gmail.com>
2023-06-07 19:35:32 +0300

    [wezterm] update global settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,5 +4,9 @@ local startup = require 'startup'
 local keys = require 'keys'
 local ui = require 'ui'
 
-local other_opts = {use_ime = false}
+local other_opts = {
+    use_ime = true,
+    -- macos_forward_to_ime_modifier_mask = 'SHIFT',
+    check_for_updates_interval_seconds = 30 * 24 * 60 * 60
+}
 return utils.merge({other_opts, startup, keys, ui})
