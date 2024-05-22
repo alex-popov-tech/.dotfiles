3146c1981251eaef63013782ec28429cc2358054
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-05-22 19:45:52 +0300

    [terminal] update ui & keymappings

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -1,166 +1,152 @@
-local vars = require 'vars'
-local wezterm = require 'wezterm'
+local vars = require("vars")
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local function activate_pane(window, pane, pane_direction, vim_direction)
-    local isViProcess = pane:get_foreground_process_name():find('n?vim') ~= nil
-    if isViProcess then
-        window:perform_action(act.SendKey({key = vim_direction, mods = 'CTRL'}),
-                              pane)
-    else
-        window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
-    end
+  local isViProcess = pane:get_foreground_process_name():find("n?vim") ~= nil
+  if isViProcess then
+    window:perform_action(act.SendKey({ key = vim_direction, mods = "CTRL" }), pane)
+  else
+    window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+  end
 end
 
-wezterm.on('activate_pane_r',
-           function(window, pane) activate_pane(window, pane, 'Right', 'l') end)
-wezterm.on('activate_pane_l',
-           function(window, pane) activate_pane(window, pane, 'Left', 'h') end)
-wezterm.on('activate_pane_u',
-           function(window, pane) activate_pane(window, pane, 'Up', 'k') end)
-wezterm.on('activate_pane_d',
-           function(window, pane) activate_pane(window, pane, 'Down', 'j') end)
+wezterm.on("activate_pane_r", function(window, pane)
+  activate_pane(window, pane, "Right", "l")
+end)
+wezterm.on("activate_pane_l", function(window, pane)
+  activate_pane(window, pane, "Left", "h")
+end)
+wezterm.on("activate_pane_u", function(window, pane)
+  activate_pane(window, pane, "Up", "k")
+end)
+wezterm.on("activate_pane_d", function(window, pane)
+  activate_pane(window, pane, "Down", "j")
+end)
 
 return {
-    disable_default_key_bindings = true,
-    leader = {key = 'F12', mods = '', timeout_milliseconds = 5000},
-    keys = {
-        {key = 'q', mods = 'SUPER', action = act.QuitApplication},
-        {key = 'v', mods = 'SUPER', action = act.PasteFrom('Clipboard')},
-        {key = 'r', mods = 'LEADER', action = act.ReloadConfiguration},
-        {key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState},
+  disable_default_key_bindings = true,
+  leader = { key = "F12", mods = "", timeout_milliseconds = 5000 },
+  keys = {
+    { key = "q", mods = "SUPER", action = act.QuitApplication },
+    { key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+    { key = "r", mods = "LEADER", action = act.ReloadConfiguration },
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 
-        {
-            key = 'x',
-            mods = 'LEADER',
-            action = act.CloseCurrentPane({confirm = true})
-        },
-        {
-            key = 'c',
-            mods = 'LEADER',
-            action = act.PromptInputLine {
-                description = 'Enter new tab name:',
-                action = wezterm.action_callback(
-                    function(window, pane, line)
-                        if line then
-                            local tab = window:mux_window():spawn_tab({
-                                cwd = wezterm.home_dir .. '/me'
-                            })
-                            tab:set_title(line)
-                            tab:activate()
-                        end
-                    end)
+    {
+      key = "x",
+      mods = "LEADER",
+      action = act.CloseCurrentPane({ confirm = true }),
+    },
+    {
+      key = "c",
+      mods = "LEADER",
+      action = act.PromptInputLine({
+        description = "Enter new tab name:",
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            local tab = window:mux_window():spawn_tab({
+              cwd = wezterm.home_dir .. "/me",
+            })
+            tab:set_title(line)
+            tab:activate()
+          end
+        end),
+      }),
+    },
 
-            }
-        },
+    { key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+    { key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+    { key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+    { key = "4", mods = "LEADER", action = act.ActivateTab(3) },
+    { key = "5", mods = "LEADER", action = act.ActivateTab(4) },
+    { key = "6", mods = "LEADER", action = act.ActivateTab(5) },
+    { key = "7", mods = "LEADER", action = act.ActivateTab(6) },
+    { key = "8", mods = "LEADER", action = act.ActivateTab(7) },
+    { key = "9", mods = "LEADER", action = act.ActivateCommandPalette },
 
-        {key = '1', mods = 'LEADER', action = act.ActivateTab(0)},
-        {key = '2', mods = 'LEADER', action = act.ActivateTab(1)},
-        {key = '3', mods = 'LEADER', action = act.ActivateTab(2)},
-        {key = '4', mods = 'LEADER', action = act.ActivateTab(3)},
-        {key = '5', mods = 'LEADER', action = act.ActivateTab(4)},
-        {key = '6', mods = 'LEADER', action = act.ActivateTab(5)},
-        {key = '7', mods = 'LEADER', action = act.ActivateTab(6)},
-        {key = '8', mods = 'LEADER', action = act.ActivateTab(7)},
-        {key = '9', mods = 'LEADER', action = act.ActivateCommandPalette},
+    -- its like '+' but to avoid pressing shift
+    { key = "=", mods = "LEADER", action = act.IncreaseFontSize },
+    { key = "-", mods = "LEADER", action = act.DecreaseFontSize },
 
-        -- its like '+' but to avoid pressing shift
-        {key = '=', mods = 'LEADER', action = act.IncreaseFontSize},
-        {key = '-', mods = 'LEADER', action = act.DecreaseFontSize},
+    {
+      key = "v",
+      mods = "LEADER",
+      action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+      key = "s",
+      mods = "LEADER",
+      action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
 
-        {
-            key = 'v',
-            mods = 'LEADER',
-            action = act.SplitHorizontal({domain = 'CurrentPaneDomain'})
-        },
-        {
-            key = 's',
-            mods = 'LEADER',
-            action = act.SplitVertical({domain = 'CurrentPaneDomain'})
-        },
+    {
+      key = "w",
+      mods = "LEADER",
+      action = wezterm.action_callback(function(window, pane)
+        local isViProcess = pane:get_foreground_process_name():find("n?vim") ~= nil
+        wezterm.log_info("leader+w pressed, isViProcess=[" .. tostring(isViProcess) .. "]")
+        if not isViProcess then
+          vars.is_resize_mode = true
+        end
+      end),
+    },
+    {
+      key = "Escape",
+      action = wezterm.action_callback(function(window, pane)
+        wezterm.log_info("Escape pressed, is_resize_mode=[" .. tostring(vars.is_resize_mode) .. "]")
+        if vars.is_resize_mode then
+          vars.is_resize_mode = false
+        else
+          return window:perform_action(act.SendKey({ key = "Escape" }), pane)
+        end
+      end),
+    },
 
-        {
-            key = 'w',
-            mods = 'LEADER',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    local isViProcess = pane:get_foreground_process_name():find(
-                                            'n?vim') ~= nil
-                    wezterm.log_info('leader+w pressed, isViProcess=[' ..
-                                         tostring(isViProcess) .. ']')
-                    if not isViProcess then
-                        vars.is_resize_mode = true
-                    end
-                end)
-        },
-        {
-            key = 'Escape',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    wezterm.log_info('Escape pressed, is_resize_mode=[' ..
-                                         tostring(vars.is_resize_mode) .. ']')
-                    if vars.is_resize_mode then
-                        vars.is_resize_mode = false
-                    else
-                        return
-                            window:perform_action(act.SendKey {key = 'Escape'},
-                                                  pane)
-                    end
-                end)
-        },
+    {
+      key = "l",
+      action = wezterm.action_callback(function(window, pane)
+        wezterm.log_info("l pressed, is_resize_mode=[" .. tostring(vars.is_resize_mode) .. "]")
+        return window:perform_action(
+          vars.is_resize_mode and act.AdjustPaneSize({ "Left", 5 }) or act.SendKey({ key = "l" }),
+          pane
+        )
+      end),
+    },
+    {
+      key = "r",
+      action = wezterm.action_callback(function(window, pane)
+        wezterm.log_info("r pressed, is_resize_mode=[" .. tostring(vars.is_resize_mode) .. "]")
+        return window:perform_action(
+          vars.is_resize_mode and act.AdjustPaneSize({ "Right", 5 }) or act.SendKey({ key = "r" }),
+          pane
+        )
+      end),
+    },
+    {
+      key = "u",
+      action = wezterm.action_callback(function(window, pane)
+        wezterm.log_info("u pressed, is_resize_mode=[" .. tostring(vars.is_resize_mode) .. "]")
+        return window:perform_action(
+          vars.is_resize_mode and act.AdjustPaneSize({ "Up", 5 }) or act.SendKey({ key = "u" }),
+          pane
+        )
+      end),
+    },
+    {
+      key = "d",
+      action = wezterm.action_callback(function(window, pane)
+        wezterm.log_info("d pressed, is_resize_mode=[" .. tostring(vars.is_resize_mode) .. "]")
+        return window:perform_action(
+          vars.is_resize_mode and act.AdjustPaneSize({ "Down", 5 }) or act.SendKey({ key = "d" }),
+          pane
+        )
+      end),
+    },
 
-        {
-            key = 'l',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    wezterm.log_info('l pressed, is_resize_mode=[' ..
-                                         tostring(vars.is_resize_mode) .. ']')
-                    return window:perform_action(
-                               vars.is_resize_mode and
-                                   act.AdjustPaneSize {'Left', 5} or
-                                   act.SendKey {key = 'l'}, pane)
-                end)
-        },
-        {
-            key = 'r',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    wezterm.log_info('r pressed, is_resize_mode=[' ..
-                                         tostring(vars.is_resize_mode) .. ']')
-                    return window:perform_action(
-                               vars.is_resize_mode and
-                                   act.AdjustPaneSize {'Right', 5} or
-                                   act.SendKey {key = 'r'}, pane)
-                end)
-        },
-        {
-            key = 'u',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    wezterm.log_info('u pressed, is_resize_mode=[' ..
-                                         tostring(vars.is_resize_mode) .. ']')
-                    return window:perform_action(
-                               vars.is_resize_mode and
-                                   act.AdjustPaneSize {'Up', 5} or
-                                   act.SendKey {key = 'u'}, pane)
-                end)
-        },
-        {
-            key = 'd',
-            action = wezterm.action_callback(
-                function(window, pane)
-                    wezterm.log_info('d pressed, is_resize_mode=[' ..
-                                         tostring(vars.is_resize_mode) .. ']')
-                    return window:perform_action(
-                               vars.is_resize_mode and
-                                   act.AdjustPaneSize {'Down', 5} or
-                                   act.SendKey {key = 'd'}, pane)
-                end)
-        },
-
-        {key = 'h', mods = 'CTRL', action = act.EmitEvent('activate_pane_l')},
-        {key = 'j', mods = 'CTRL', action = act.EmitEvent('activate_pane_d')},
-        {key = 'k', mods = 'CTRL', action = act.EmitEvent('activate_pane_u')},
-        {key = 'l', mods = 'CTRL', action = act.EmitEvent('activate_pane_r')}
-    }
+    { key = "h", mods = "CTRL", action = act.EmitEvent("activate_pane_l") },
+    { key = "j", mods = "CTRL", action = act.EmitEvent("activate_pane_d") },
+    { key = "k", mods = "CTRL", action = act.EmitEvent("activate_pane_u") },
+    { key = "l", mods = "CTRL", action = act.EmitEvent("activate_pane_r") },
+  },
 }
--- wezterm/.config/wezterm/ui.lua
+++ wezterm/.config/wezterm/ui.lua
@@ -1,91 +1,102 @@
-local vars = require'vars'
-local wezterm = require 'wezterm'
+local vars = require("vars")
+local wezterm = require("wezterm")
 
 local format_title = function(title, is_active, max_width)
-    local background = {Background = {Color = '#1f1f28'}}
-    local title_len = #title
-    local pad_len = math.floor((max_width - title_len) / 2)
+  local background = { Background = { Color = "#1f1f28" } }
+  local title_len = #title
+  local pad_len = math.floor((max_width - title_len) / 2)
 
-    local formatted_title = {
-        Text = string.rep(' ', pad_len) .. title .. string.rep(' ', pad_len)
-    }
-    if is_active then
-        return {background, {Foreground = {Color = '#957fb8'}}, formatted_title}
-    else
-        return {background, {Foreground = {Color = '#cad3f5'}}, formatted_title}
-    end
+  local formatted_title = {
+    Text = string.rep(" ", pad_len) .. title .. string.rep(" ", pad_len),
+  }
+  if is_active then
+    return { background, { Foreground = { Color = "#957fb8" } }, formatted_title }
+  else
+    return { background, { Foreground = { Color = "#cad3f5" } }, formatted_title }
+  end
 end
 
-local user_var_tab_title_key = 'tab_title';
-wezterm.on('format-tab-title',
-           function(tab, tabs, panes, config, hover, max_width)
-    -- if there is title already set, proceed with it
-    if type(tab.tab_title) == 'string' and #tab.tab_title > 0 then
-        return format_title(tab.tab_title, tab.is_active, max_width)
-    end
-    return format_title('temp', tab.is_active, max_width)
+local user_var_tab_title_key = "tab_title"
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  -- if there is title already set, proceed with it
+  if type(tab.tab_title) == "string" and #tab.tab_title > 0 then
+    return format_title(tab.tab_title, tab.is_active, max_width)
+  end
+  return format_title("temp", tab.is_active, max_width)
 end)
 
-wezterm.on('update-right-status', function(window)
-    local date = wezterm.strftime '%Y-%m-%d %H:%M:%S'
-    local mode = vars.is_resize_mode and '>>RESIZE<< ' or ''
-    window:set_right_status(mode .. date)
+wezterm.on("update-right-status", function(window)
+  local date = wezterm.strftime("%Y-%m-%d %H:%M:%S")
+  local mode = vars.is_resize_mode and ">>RESIZE<< " or ""
+  window:set_right_status(mode .. date)
 end)
 
-wezterm.on('user-var-changed', function(window, pane, name, value)
-    wezterm.log_info('user-var-changed', name, value)
-    if name == user_var_tab_title_key then pane:tab():set_title(value) end
+wezterm.on("user-var-changed", function(window, pane, name, value)
+  wezterm.log_info("user-var-changed", name, value)
+  if name == user_var_tab_title_key then
+    pane:tab():set_title(value)
+  end
 end)
 
 return {
-    font = wezterm.font 'JetBrains Mono',
-    font_size = 14,
-    adjust_window_size_when_changing_font_size = false,
-    -- dpi = 144.0,
-    tab_max_width = 14,
-    colors = {
-        tab_bar = {
-            -- The color of the inactive tab bar edge/divider
-            background = '#1f1f28',
-            new_tab = {bg_color = '#1f1f28', fg_color = '#dcd7ba'}
-        },
-        foreground = '#dcd7ba',
-        background = '#1f1f28',
-
-        cursor_bg = '#c8c093',
-        cursor_fg = '#c8c093',
-        cursor_border = '#c8c093',
-
-        selection_fg = '#c8c093',
-        selection_bg = '#2d4f67',
-
-        scrollbar_thumb = '#16161d',
-        split = '#16161d',
-
-        ansi = {
-            '#090618',
-            '#c34043',
-            '#76946a',
-            '#c0a36e',
-            '#7e9cd8',
-            '#957fb8',
-            '#6a9589',
-            '#c8c093'
-        },
-        brights = {
-            '#727169',
-            '#e82424',
-            '#98bb6c',
-            '#e6c384',
-            '#7fb4ca',
-            '#938aa9',
-            '#7aa89f',
-            '#dcd7ba'
-        },
-        indexed = {[16] = '#ffa066', [17] = '#ff5d62'}
+  font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font({
+        family = "JetBrains Mono",
+        weight = "DemiBold",
+        italic = true,
+      }),
     },
-    window_decorations = 'RESIZE',
-    -- window_background_opacity = 0.9,
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false
+  },
+  font_size = 16,
+  adjust_window_size_when_changing_font_size = false,
+  -- dpi = 144.0,
+  tab_max_width = 14,
+  colors = {
+    tab_bar = {
+      -- The color of the inactive tab bar edge/divider
+      background = "#1f1f28",
+      new_tab = { bg_color = "#1f1f28", fg_color = "#dcd7ba" },
+    },
+    foreground = "#dcd7ba",
+    background = "#1f1f28",
+
+    cursor_bg = "#c8c093",
+    cursor_fg = "#c8c093",
+    cursor_border = "#c8c093",
+
+    selection_fg = "#c8c093",
+    selection_bg = "#2d4f67",
+
+    scrollbar_thumb = "#16161d",
+    split = "#16161d",
+
+    ansi = {
+      "#090618",
+      "#c34043",
+      "#76946a",
+      "#c0a36e",
+      "#7e9cd8",
+      "#957fb8",
+      "#6a9589",
+      "#c8c093",
+    },
+    brights = {
+      "#727169",
+      "#e82424",
+      "#98bb6c",
+      "#e6c384",
+      "#7fb4ca",
+      "#938aa9",
+      "#7aa89f",
+      "#dcd7ba",
+    },
+    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+  },
+  window_decorations = "RESIZE",
+  window_background_opacity = 0.98,
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
 }
