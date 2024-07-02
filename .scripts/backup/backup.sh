550fdad0e095860491bf23a84bfa5047df9fca8b
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-07-03 01:49:46 +0300

    [scripts] remove old scripts

--- .scripts/backup/backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-$HOME/.dotfiles/.scripts/backup/notifyOnErr.sh $HOME/.dotfiles/.scripts/backup/lastpass.sh
--- .scripts/backup/backupWithNotify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-terminal-notifier -sound default \
-  -title "Start backing up your passwords?" \
-  -message "Make sure LastPass is authorized and click on this notification" \
-  -execute "zsh $HOME/.dotfiles/.scripts/backup/backup.sh"
