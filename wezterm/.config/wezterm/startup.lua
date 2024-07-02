96354219e3b8bd1a9d48f30eafa75b436b3ce051
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-07-02 17:38:13 +0300

    [wezterm] update startup folders

--- wezterm/.config/wezterm/startup.lua
+++ wezterm/.config/wezterm/startup.lua
@@ -1,37 +1,36 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local mux = wezterm.mux
 
-wezterm.on('gui-startup', function(cmd)
-    -- allow `wezterm start -- something` to affect what we spawn
-    -- in our initial window
-    local args = {}
-    if cmd then args = cmd.args end
+wezterm.on("gui-startup", function(cmd)
+  -- allow `wezterm start -- something` to affect what we spawn
+  -- in our initial window
+  local args = {}
+  if cmd then
+    args = cmd.args
+  end
 
-    local home = wezterm.home_dir
+  local home = wezterm.home_dir
 
-    local stats_tab, stats_pane, window = mux.spawn_window {
-        workspace = 'default',
-        cwd = home .. '/.dotfiles'
-    }
-    -- window:gui_window():maximize()
-    stats_pane:send_text('btop\n')
-    stats_tab:set_title('stats')
-    wezterm.log_info(stats_pane:get_foreground_process_name())
+  local stats_tab, stats_pane, window = mux.spawn_window({
+    workspace = "default",
+    cwd = home .. "/.dotfiles",
+  })
+  -- window:gui_window():maximize()
+  stats_pane:send_text("btop\n")
+  stats_tab:set_title("stats")
+  wezterm.log_info(stats_pane:get_foreground_process_name())
 
-    local dotfiles_tab = window:spawn_tab({
-        args = args,
-        cwd = home .. '/.dotfiles'
-    })
-    dotfiles_tab:set_title('dotfiles')
+  local dotfiles_tab = window:spawn_tab({
+    args = args,
+    cwd = home .. "/.dotfiles",
+  })
+  dotfiles_tab:set_title("dotfiles")
 
-    local kc_tab = window:spawn_tab({cwd = home .. '/me/work/informa'})
-    kc_tab:set_title('K&C')
+  local kc_tab = window:spawn_tab({ cwd = home .. "/me/work/informa" })
+  kc_tab:set_title("K&C")
 
-    local frontend_tab = window:spawn_tab({cwd = home .. '/me/reporter'})
-    frontend_tab:set_title('RIP')
-
-    window:gui_window():perform_action(act.ActivateTab(0), stats_pane)
+  window:gui_window():perform_action(act.ActivateTab(0), stats_pane)
 end)
 
 return {}
