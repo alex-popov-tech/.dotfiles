fc9f5c3f564c0d4bdb5fe4adc1d80403fd428a82
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-12-12 23:51:36 +0200

    [misc] update linkconfigs.sh

--- .scripts/linkconfigs.sh
+++ .scripts/linkconfigs.sh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 cd $HOME/.dotfiles
 
-/bin/ls -d */ | xargs -n1 stow
-
-cp -f .other/fonts/* $HOME/Library/Fonts/
-defaults import com.manytricks.Moom .other/Moom.plist
+/bin/ls -d */ | xargs -n1 stow --restow
 
 cd -
