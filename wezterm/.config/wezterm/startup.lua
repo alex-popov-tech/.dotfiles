038cb3af281e21b3b209e2c4da2b76fd31232eb2
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-12-12 23:37:41 +0200

    [wezterm] update stats window

--- wezterm/.config/wezterm/startup.lua
+++ wezterm/.config/wezterm/startup.lua
@@ -16,7 +16,7 @@ wezterm.on("gui-startup", function(cmd)
     workspace = "default",
     cwd = home .. "/.dotfiles",
   })
-  stats_pane:split({ args = { "up" }, direction = "Right", size = 20 })
+  stats_pane:split({ args = { "/Users/alex/.go/bin/up" }, direction = "Right", size = 20 })
   -- window:gui_window():maximize()
   stats_pane:send_text("btop\n")
   stats_tab:set_title("stats")
@@ -30,6 +30,9 @@ wezterm.on("gui-startup", function(cmd)
 
   local kc_tab = window:spawn_tab({ cwd = home .. "/me/work/informa" })
   kc_tab:set_title("K&C")
+
+  local onlyfans_tab = window:spawn_tab({ cwd = home .. "/me/work/of" })
+  onlyfans_tab:set_title("of")
 
   window:gui_window():perform_action(act.ActivateTab(0), stats_pane)
 end)
